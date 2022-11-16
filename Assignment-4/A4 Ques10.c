e6642f259f8d0073cdf4cdba230c285003f044a3
BhodraEnjoy <enjoy.bhodra02@gmail.com>
2022-11-16 11:37:10 +0600

    Assignment-4

---
+++ Assignment-4/A4 Ques10.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+
+    int a = 5;
+    for(int i = 1; i <= 10; i++)
+    {
+        printf("%d X %d = %d\n",a,i,a*i);
+    }
+    return 0;
+}
---
+++ Assignment-4/A4 Ques5.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 20;
+    while(i >= 1)
+    {
+        if(i % 2 == 1)
+            printf("%d ", i);
+            i--;
+    }
+
+    return 0;
+}
+
